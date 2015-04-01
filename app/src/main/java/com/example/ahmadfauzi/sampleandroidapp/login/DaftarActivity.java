79157e91c2f4881921114ad87a4526a9134a0255
ahmadfauzi <ahmadfauzi.alwahid@gmail.com>
2015-04-01 18:53:48 +0700

    ambil gambar dari gallery

--- app/src/main/java/com/example/ahmadfauzi/sampleandroidapp/login/DaftarActivity.java
+++ app/src/main/java/com/example/ahmadfauzi/sampleandroidapp/login/DaftarActivity.java
@@ -1,14 +1,29 @@
 package com.example.ahmadfauzi.sampleandroidapp.login;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.ahmadfauzi.sampleandroidapp.R;
 
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class DaftarActivity extends ActionBarActivity {
+
+    public int GALLERY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,5 +67,46 @@ public class DaftarActivity extends ActionBarActivity {
 
     public void batal(View view) {
         finish();
+    }
+
+    public void takeFromGallery(View view) {
+        EditText nipDosen = (EditText) findViewById(R.id.editTextNIP);
+        if(nipDosen.getText().toString().isEmpty()){
+            Toast.makeText(this,"Mohon isi NIP dahulu!", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Pilih foto"), GALLERY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap fotoDosen = null;
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_REQUEST_CODE){
+                Uri selectedImageUri = data.getData();
+                try{
+                    ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(selectedImageUri, "r");
+                    FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                    Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                    parcelFileDescriptor.close();
+
+                    String fileFotoDosen = selectedImageUri.toString();
+                    ImageView mIvFotoDosen = (ImageView) findViewById(R.id.imageView);
+                    mIvFotoDosen.setImageBitmap(image);
+                    Log.d("DaftarActivity","Foto Dosen berhasil diambil dari: " + fileFotoDosen.toString());
+                    Toast.makeText(this,"Foto Dosen berhasil diambil: " + fileFotoDosen.toString(), Toast.LENGTH_SHORT).show();
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
