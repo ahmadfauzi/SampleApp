4845ac4197bf12387c85f5918d9eb3ab7ef49a2f
Ahmad Fauzi <ahmadfauzi.alwahid@gmail.com>
2015-04-02 19:47:52 +0700

    ambil foto dari camera

--- app/src/main/java/com/example/ahmadfauzi/sampleandroidapp/login/DaftarActivity.java
+++ app/src/main/java/com/example/ahmadfauzi/sampleandroidapp/login/DaftarActivity.java
@@ -3,8 +3,10 @@ package com.example.ahmadfauzi.sampleandroidapp.login;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,7 +26,7 @@ import java.io.IOException;
 public class DaftarActivity extends ActionBarActivity {
 
     public int GALLERY_REQUEST_CODE = 1;
-
+    public int CAMERA_REQUEST_CODE = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,6 +83,16 @@ public class DaftarActivity extends ActionBarActivity {
         }
     }
 
+    public void takeFromCamera(View view) {
+        EditText nipDosen = (EditText) findViewById(R.id.editTextNIP);
+        if(nipDosen.getText().toString().isEmpty()){
+            Toast.makeText(this, "Mohon isi NIP dulu !", Toast.LENGTH_SHORT).show();
+        }else{
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        }
+    }
+
     @Override
     protected  void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
@@ -106,7 +118,19 @@ public class DaftarActivity extends ActionBarActivity {
                 } catch (IOException e){
                     e.printStackTrace();
                 }
+            }else if(requestCode == CAMERA_REQUEST_CODE){
+                Bitmap bm = (Bitmap) data.getExtras().get("data");
+                if(bm != null){
+                    Log.d("DaftarActivity", "Mengambil foto dari kamera berhasil");
+                    fotoDosen = ThumbnailUtils.extractThumbnail(bm, 300, 300);
+                    ImageView mIvFotoDosen = (ImageView) findViewById(R.id.imageView);
+                    mIvFotoDosen.setImageBitmap(fotoDosen);
+                }else{
+                    Log.d("DaftarActivity", "Mengambil foto dari kamera gagal");
+                }
             }
         }
     }
+
+
 }
